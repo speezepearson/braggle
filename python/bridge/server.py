eff7170eb6a284e5f21e14e5c4abd820163e320d
Spencer Pearson <spencerpearson@dropbox.com>
2019-07-23 02:03:36 -0700

    run on localhost on automatic port; open browser automatically

--- python/bridge/server.py
+++ python/bridge/server.py
@@ -1,5 +1,9 @@
 import asyncio
 import functools
+import socket
+import threading
+import time
+import webbrowser
 
 from pathlib import Path
 from typing import Iterable, MutableSet, Optional, Sequence, Set, TypeVar
@@ -45,7 +49,26 @@ def build_routes(
         web.RouteDef(method='POST', path='/interaction', handler=functools.partial(interaction, gui=gui, condition=condition), kwargs={}),
     ]
 
-def serve(gui: AbstractGUI, *, loop: Optional[asyncio.AbstractEventLoop] = None) -> None:
+def _get_open_port() -> int:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('localhost', 0))
+    port = s.getsockname()[1]
+    s.close()
+    return port
+def _open_page_soon(url: str, delay_sec: float = 0.1) -> None:
+    def f() -> None:
+        time.sleep(delay_sec)
+        webbrowser.open(url)
+    threading.Thread(target=f).start()
+
+def serve(
+    gui: AbstractGUI,
+    *,
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+    host: str = 'localhost',
+    port: Optional[int] = None,
+    open_browser: bool = True,
+) -> None:
     loop_ = loop if (loop is not None) else asyncio.get_event_loop()
     condition = asyncio.Condition(loop=loop_)
     async def notify_all():
@@ -54,4 +77,8 @@ def serve(gui: AbstractGUI, *, loop: Optional[asyncio.AbstractEventLoop] = None)
     gui.add_listener(lambda: asyncio.run_coroutine_threadsafe(notify_all(), loop_))
     app = web.Application(loop=loop_)
     app.add_routes(build_routes(gui=gui, condition=condition))
-    web.run_app(app, port=4392)
+    if port is None:
+        port = _get_open_port()
+    if open_browser:
+        _open_page_soon(f'http://localhost:{port}')
+    web.run_app(app, host=host, port=port)
